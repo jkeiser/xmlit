ef2a4597ba8e5f026e8ef77944c5f2e1417d997b
John Keiser <jkeiser@opscode.com>
2014-07-13 22:10:22 -0700

    Add godoc

--- decode_elements.go
+++ decode_elements.go
@@ -5,19 +5,44 @@ import "encoding/xml"
 import "reflect"
 import "github.com/jkeiser/iter"
 
+// Used to specify the list of XML names and value factories used to parse the
+// stream of XML.
 type CreatorMap map[xml.Name]func() interface{}
 
-// iterator := xmlit.DecodeElements(reader, Customer{}, Order{}, Account{})
+// Returns an iterator that uses xml.Decoder to decode a strema of the given types.
+// Will ignore any containing tags, skipping straight to the actual elements in
+// question.
+//
+// The input is a list of "exemplars."  DecodeElements will analyze their types
+// and use the type names to decide what XML element tags to read.  A new value
+// of the type will be created for each decoded element.
+//
+// The input stream will be closed after iteration.
+//
+//   iterator := xmlit.DecodeElements(reader, Customer{}, Order{}, Account{})
+//   iterator.Each(func (item interface{}) { fmt.Println(item) })
 func DecodeElements(reader io.ReadCloser, exemplars ...interface{}) iter.Iterator {
 	return iterateElements(reader, decodeElementsFunc(exemplars))
 }
 
-// iterator := xmlit.DecodeElementsByXmlName(reader, CreatorMap{
-//   "Customer": func() { Customer{} },
-//   "Order": func() { Order{} },
-//   "DelinquentOrder": func() { DelinquentOrder{} },
-//   "Account": func() { Account{} },
-// })
+// Returns an iterator that uses xml.Decoder to decode a strema of the given types.
+// Will ignore any containing tags, skipping straight to the actual elements in
+// question.
+//
+// The CreatorMap lets you specify a map of element names to value factories: the
+// value factories return pointers to new objects which will be passed to
+// xml.Decoder.DecodeElement() when <TypeName> is seen in the XML.  Generally you
+// will use new() to get the pointer.
+//
+// The input stream will be closed after iteration.
+//
+//   iterator := xmlit.DecodeElementsByXmlName(reader, CreatorMap{
+//     "Customer":        func() interface{} { new(Customer) },
+//     "Order":           func() interface{} { new(Order) },
+//     "DelinquentOrder": func() interface{} { new(DelinquentOrder) },
+//     "Account":         func() interface{} { new(Account) },
+//   })
+//   iterator.Each(func (item interface{}) { fmt.Println(item) })
 func DecodeElementsByXmlName(reader io.ReadCloser, creators CreatorMap) iter.Iterator {
 	return iterateElements(reader, decodeElementsByXmlNameFunc(creators))
 }
